7b8c2b06cd6109f7a90e508acaba97f1b1255748
wsnkrnwn12 <wisnukurniawan38@gmail.com>
2020-04-13 15:18:50 +0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -5,4 +5,13 @@ public class OperatorBinary {
     String nama = "Wisnu Kurniawan";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 4.00;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
