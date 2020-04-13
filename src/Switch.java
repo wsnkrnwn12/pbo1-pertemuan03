ffcae4e7ce4680a44445d9a2c9575dbe78c2c057
wsnkrnwn12 <wisnukurniawan38@gmail.com>
2020-04-13 16:12:12 +0700

    percabangan Switch

---
+++ src/Switch.java
@@ -0,0 +1,27 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int Enemy=5;
+        switch(Enemy){
+            case 1:
+                System.out.println("Crocodile");
+                break;
+            case 2:
+                System.out.println("RobLucci");
+                break;
+            case 3:
+                System.out.println("GeckoMoria");
+                break;
+            case 4:
+                System.out.println("CaesarClown");
+                break;
+            case 5:
+                System.out.println("Doflamingo");
+                break;
+            default:
+                System.out.println("Katakuri");
+                break;
+        }
+    }
+}
